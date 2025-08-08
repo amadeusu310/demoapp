356d86a2fd94464f29aa2d6e543c02ecfd21a990
saya_38_t <sayabear30@gmail.com>
2025-08-08 20:48:25 +0900

    カレンダー作成、エラー修正作業前のバックアップ

--- app/routes/calendar.tsx
+++ app/routes/calendar.tsx
@@ -2,7 +2,11 @@ import { Link } from "react-router";
 import type { Route } from "./+types/calendar";
 import Header from "~/components/Header";
 import Footer from "~/components/Footer";
-import { getCurrentUser } from "~/utils/supabaseClient";
+import { getTasks } from "~/utils/supabaseClient";
+import { useState, useEffect } from "react";
+import Gantt from "react-frappe-gantt";
+import type {GanttTask, DbTask} from "~/types/react-frappe-gantt";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,6 +15,41 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function calendar(){
-  
+export default function Calendar(){
+  const [tasks,setTasks] = useState<GanttTask[]>([]);//型指定
+
+  useEffect(()=>{
+    (async () => {
+      const data: DbTask[] = await getTasks();
+
+      const formatted: GanttTask[] = data.map(task =>({  //ガントチャート
+        id: String(task.id),
+        name: task.title,
+        start: typeof task.created_at==="string" 
+          ? task.created_at 
+          : task.created_at
+          ? task.created_at.toISOString().slice(0,10) 
+          : "",
+        end: typeof task.period==="string"
+          ? task.period
+          : task.period
+          ? task.period.toISOString().slice(0,10)
+          : "",
+        progress: task.completed ? 100:0, //完了なら100%, 未完了は0%
+      }));
+      setTasks(formatted);
+    })();
+  }, []);
+
+  return(
+    <div className="flex flex-col h-screen bg-green-50">
+      <Header />
+      <main className="flex-grow pt-20 pb-20 overflow-y-auto p-4">
+        {tasks.length > 0 ? (
+          <Gantt tasks={tasks} viewMode="Day"/>
+        ):(<p>読み込み中...</p>)}
+      </main>
+      <Footer />
+    </div>
+  )
 }
